fe9fe620af23cdf02f61f0bb0b7b7951222653f0
@johnya0 <anoopjohny2000@gmail.com>
2023-01-26 03:58:18 +0100

    my flask web save

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 posts=[
 
@@ -30,6 +31,8 @@ posts=[
 ]
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -38,6 +41,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__=="__main__":
     app.run(debug=True)
