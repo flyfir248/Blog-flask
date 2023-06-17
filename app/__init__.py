dffd6c6527b70c963dd47b76bbeb1fcfcb261e76
@johnya0 <anoopjohny2000@gmail.com>
2023-06-17 13:55:56 +0200

    my flask web save

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy # temporary database for storing information
+
+
+
+app = Flask(__name__)
+# the configuration variables are usually defined as uppercase strings.
+# This is a convention to help distinguish them from other variables in the code.
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# 3 slashes for the relative path to the place where u want the db to be created
+db=SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+#C:\Users\anoop\PycharmProjects\FlaskBlog
+import sqlite3
+
+from app import routes
