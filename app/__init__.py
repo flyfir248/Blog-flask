5f85d8b5f455b8d0a1e5f0f5bb0900eb69497864
@johnya0 <anoopjohny2000@gmail.com>
2023-06-17 15:18:19 +0200

    my flask web save

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy # temporary database for storing information
-
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # the configuration variables are usually defined as uppercase strings.
@@ -10,6 +10,9 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # 3 slashes for the relative path to the place where u want the db to be created
 db=SQLAlchemy(app)
+bcrypt =Bcrypt
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 with app.app_context():
     db.create_all()
