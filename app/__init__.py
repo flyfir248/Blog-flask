a75e91a4bb28b22298b6a3908c5b88080e85212e
@johnya0 <anoopjohny2000@gmail.com>
2023-06-26 21:59:02 +0200

    my flask web save

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # 3 slashes for the relative path to the place where u want the db to be created
-db=SQLAlchemy(app)
-bcrypt =Bcrypt
+db = SQLAlchemy(app)
+bcrypt =Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'  # the style
