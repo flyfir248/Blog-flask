d840ee5b0dd733417ae81ee49b807b411911349f
@johnya0 <anoopjohny2000@gmail.com>
2023-06-30 12:31:48 +0200

    my flask web save

--- app/config.py
+++ app/config.py
@@ -2,8 +2,18 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    # app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+    # app.config['MAIL_PORT'] = 587
+    # app.config['MAIL_USE_TLS'] = True
+    # app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+    # app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
