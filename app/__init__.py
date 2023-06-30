8ecd7349c4ec75c94743038d31f936841fe19550
@johnya0 <anoopjohny2000@gmail.com>
2023-06-30 11:42:02 +0200

    my flask web save

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy # temporary database for storing information
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 # the configuration variables are usually defined as uppercase strings.
@@ -14,6 +16,12 @@ bcrypt =Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'  # the style
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 with app.app_context():
     db.create_all()
