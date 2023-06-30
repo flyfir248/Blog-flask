c785f2f32b98afcab443061302219aab495aec56
@johnya0 <anoopjohny2000@gmail.com>
2023-06-30 16:43:18 +0200

    my flask web save

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy # temporary database for storing information
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -13,16 +13,20 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # 3 slashes for the relative path to the place where u want the db to be created
 
-db = SQLAlchemy(app)
-bcrypt =Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'  # the style
+login_manager.login_message_category = 'info'
+mail = Mail()  # the style
 #app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 #app.config['MAIL_PORT'] = 587
 #app.config['MAIL_USE_TLS'] = True
 #app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 #app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+
+
 mail = Mail(app)
 
 def create_app(config_class=Config):
---
+++ bash_procfile
@@ -0,0 +1,2 @@
+export SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+export SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
